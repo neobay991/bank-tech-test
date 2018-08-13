e77ce8d7a7c47f3142ce1ed6d1391c1f56b987a5
Jay Khan <jkhan992@gmail.com>
2018-08-13 13:33:02 +0100

    TESTS Add unit tests for User

---
+++ jasmine/jasmine-standalone-3.1.0/spec/UserSpec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe("Feature Test: ", function () {
+  var user;
+  var account;
+
+  beforeEach(function(){
+    user = new User();
+    account = new Account();
+  });
+
+  describe('User'), function(){
+    it('A User has an account with a zero balance'), function() {
+      expect(user.account._customerBalance).toEqual(0);
+    )};
+  )};
+
+});
