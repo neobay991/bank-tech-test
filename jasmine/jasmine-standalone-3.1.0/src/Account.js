e40b2e922fa0859afafdbacabb392032284bff66
Jay Khan <jkhan992@gmail.com>
2018-08-13 15:15:31 +0100

    FEATURE Create function to allow user to withdraw money from their account

--- jasmine/jasmine-standalone-3.1.0/src/Account.js
+++ jasmine/jasmine-standalone-3.1.0/src/Account.js
@@ -12,6 +12,13 @@ Account.prototype.deposit = function(amount) {
   this._customerDepositWithdrawalTimeStamp.push(timestamp);
 }
 
+Account.prototype.withdraw = function(amount) {
+  var timestamp = Date.now();
+
+  this._customerBalance.push(amount);
+  this._customerDepositWithdrawalTimeStamp.push(timestamp);
+}
+
 Account.prototype.balance = function() {
   var counter = 0,
   balance = 0;
