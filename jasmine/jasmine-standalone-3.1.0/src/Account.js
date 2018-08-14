5beeefeea767ec92b9c5ff969e63dfc2cb688c84
Jay Khan <jkhan992@gmail.com>
2018-08-14 12:16:06 +0100

    OTHER REFACTOR console log output for statement

--- jasmine/jasmine-standalone-3.1.0/src/Account.js
+++ jasmine/jasmine-standalone-3.1.0/src/Account.js
@@ -50,7 +50,6 @@ Account.prototype.statement = function() {
   console.log(output);
   for (var i = 1; i < this._customerDepositWithdrawalTimeStamp.length; ++i) {
     entry = (this._customerDepositWithdrawalTimeStamp[i] + " " + this._customerDeposit[i] + " " + this._customerBalance[i])
-     // Remove this comment to print to console
      console.log(entry);
   }
   return output + " " + entry;
