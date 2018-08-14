73bc63c04ff4da7913404d816d5d3e3d0fae1548
Jay Khan <jkhan992@gmail.com>
2018-08-14 15:10:45 +0100

    SETUP update Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,11 +3,11 @@ module.exports = function(config) {
     basePath: '../..',
     frameworks: ['jasmine'],
     preprocessors: {
-      '/Users/mjawadkhan/Documents/Projects/tech_tests/bank-tech-test/jasmine/jasmine-standalone-3.1.0/src/*.js': 'coverage'
+      'jasmine/jasmine-standalone-3.1.0/src/*.js': 'coverage'
     },
     reporters: ['coverage'],
     files: [
-      '/Users/mjawadkhan/Documents/Projects/tech_tests/bank-tech-test/jasmine/jasmine-standalone-3.1.0/spec/*.js',
+      'jasmine/jasmine-standalone-3.1.0/spec/*.js',
     ],
     plugins: [ 'karma-jasmine', 'karma-coverage' ],
     //...
