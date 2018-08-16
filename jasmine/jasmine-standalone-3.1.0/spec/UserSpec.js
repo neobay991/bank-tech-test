23c17ccae8ebbd539fed26da473dce173d989f27
Jay Khan <jkhan992@gmail.com>
2018-08-16 04:14:30 +0100

    TESTS Mocked Account

--- jasmine/jasmine-standalone-3.1.0/spec/UserSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/UserSpec.js
@@ -6,35 +6,44 @@ describe("Unit Test: ", function () {
   var timerCallback;
 
   beforeEach(function(){
-    user = new User(account = new Account);
+    account = jasmine.createSpyObj('account', ['deposit', 'withdraw', 'timeStamp', 'balance', 'balanceTimeStamp', 'updateBalance', 'statement', 'printStatement']);
+    user = new User(account);
     timerCallback = jasmine.createSpy("timerCallback");
     jasmine.clock().uninstall();
     jasmine.clock().install();
   });
 
   describe('User', function(){
+    beforeEach(function(){
+      account.deposit.and.returnValue(2000);
+      account.withdraw.and.returnValue(1000);
+      account.balance.and.returnValue(2000);
+      account.printStatement.and.returnValue('date || credit / debit || balance 13/8/2018 -1000 1000,13/8/2018 2000 2000')
+      account._customerBalance = [0];
+    });
+
     it('A User has an account with a zero balance', function() {
       expect(user._account._customerBalance).toEqual([0]);
     });
 
-    it('A User has an account with a balance of 1000', function() {
-      user.depositMoney(1000);
-      expect(user.getBalance()).toEqual(1000);
+    it('#deposit', function() {
+      user.depositMoney(2000);
+      expect(user.getBalance()).toEqual(2000);
     });
 
-    it('A User has an account with a balance of 500', function() {
-      user.depositMoney(1000);
-      user.withdrawMoney(-500);
-      expect(user.getBalance()).toEqual(500);
+    it('#withdraw', function() {
+      user.depositMoney(3000);
+      user.withdrawMoney(-1000);
+      expect(user.getBalance()).toEqual(2000);
     });
 
     it('A User can get an account statement', function() {
       var baseTime = new Date(2018, 7, 13);
       jasmine.clock().mockDate(baseTime);
-      jasmine.clock().tick(50);
-      user.depositMoney(1000);
-      user.withdrawMoney(-500);
-      expect(user.getAccountStatement()).toEqual('date || credit / debit || balance 13/8/2018 -500 500,13/8/2018 1000 1000');
+      jasmine.clock().tick(50000);
+      user.depositMoney(2000);
+      user.withdrawMoney(-1000);
+      expect(user.getAccountStatement()).toEqual('date || credit / debit || balance 13/8/2018 -1000 1000,13/8/2018 2000 2000');
     });
   });
 
